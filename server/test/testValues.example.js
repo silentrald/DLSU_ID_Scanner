e43187c02009b9caa846ca39b910e753af4ce6ea
NeilMatthewLua <neil_lua@dlsu.edu.ph>
2020-12-06 22:17:10 +0800

    commented out all test scripts

--- server/test/testValues.example.js
+++ server/test/testValues.example.js
@@ -1,21 +1,21 @@
 
-// Change the values
-const values = {
-    admin: {
-        username: '',
-        password: '',
-    },
-    organizer: {
-        username: '',
-        password: '',
-    },
-    checker: {
-        username: '',
-        password: '',
-    },
-    eventID: '',
-    serialID: '',
-    newSerialID: '00000000',
-};
+// // Change the values
+// const values = {
+//     admin: {
+//         username: '',
+//         password: '',
+//     },
+//     organizer: {
+//         username: '',
+//         password: '',
+//     },
+//     checker: {
+//         username: '',
+//         password: '',
+//     },
+//     eventID: '',
+//     serialID: '',
+//     newSerialID: '00000000',
+// };
 
-module.exports = values;
+// module.exports = values;
